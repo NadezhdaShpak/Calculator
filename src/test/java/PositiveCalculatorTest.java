9a49a641b16f1fa669038cdcb82c95aa5a54ee48
shpak <ket180@yandex.ru>
2025-09-19 11:08:11 +0300

    ADD positive test

--- src/test/java/PositiveCalculatorTest.java
+++ src/test/java/PositiveCalculatorTest.java
@@ -1,2 +1,34 @@
+import model.Calculator;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
 public class PositiveCalculatorTest {
+
+    @DataProvider
+    public Object[][] positiveData(){
+        return new Object[][] {
+                {"+", "2", "3", 5.0},
+                {"-", "5", "3", 2.0},
+                {"*", "4", "5", 20.0},
+                {"/", "10", "2", 5.0},
+                {"+", "0", "0", 0.0},
+                {"-", "0", "5", -5.0},
+                {"*", "0", "100", 0.0},
+                {"/", "7", "2", 3.5},
+                {"+", "-2", "3", 1.0},
+                {"-", "-5", "-3", -2.0},
+                {"*", "-4", "5", -20.0},
+                {"/", "-10", "2", -5.0}
+        };
+    }
+
+    @Test (dataProvider = "positiveData")
+    public void positiveTest(String operator, String first, String second, double expected){
+        String[] params = {operator, first, second};
+        String res = Calculator.execute(params);
+        double actual = Double.parseDouble(res);
+        Assert.assertEquals(actual, expected);
+    }
 }
